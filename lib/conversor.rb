b47b47cf23ac9c7642d3c7138bc72b09da53b2a6
Nerian <siotopo@gmail.com>
2010-10-23 21:41:44 +0200

    Refactor the perform_operations method in several methods. Add a lot of incode documentation

--- lib/conversor.rb
+++ lib/conversor.rb
@@ -3,11 +3,11 @@ require 'find'
 module Conversor
    class Conversor
                      
-     #output is used for Cucumber testing.
-     #svn_address_origin is the online address of the svn repo that we can to mirror from
-     #svn_address_destiny is the online address of the svn repo that we want to mirror to  
-     #svn_origin_name is the name of the checkout origin repo.
-     #svn_destiny_name is the name of the checkout destiny repo.
+     # output is used for Cucumber testing.
+     # svn_address_origin is the online address of the svn repo that we can to mirror from
+     # svn_address_destiny is the online address of the svn repo that we want to mirror to  
+     # svn_origin_name is the name of the checkout origin repo.
+     # svn_destiny_name is the name of the checkout destiny repo.
      attr_accessor :output, :svn_address_origin, :svn_address_destiny, :svn_origin_name, :svn_destiny_name, :final_revision_that_you_want_to_mirror
       
      def initialize(output=STDOUT, svn_address_origin, svn_address_destiny)
@@ -74,77 +74,109 @@ While the revision of both origin and destiny repo are not the same:
          revision_that_we_want_the_destiny_to_be_in = revision_that_we_want_the_destiny_to_be_in + 1 
          puts "\n--------------\n"
        end
-     end                                
+     end
+     
+     
+     def remove_files_from_destiny_repo_that_were_removed_in_origin_repo()
+       # SVN Remove files that are in destiny but are not in origin 
+       list_of_files_that_should_be_removed = []
+       puts "\n--> Removing files that exist in destiny but are not in origin, which means they were removed and should be scheduled svn rm 'file'\n"
+       Dir.glob(File.join("/tmp/#{@svn_destiny_name}", '**', '*')) do |file_path_destiny|
+         if not file_path_destiny.include?(".svn")
+           file_path_origin = file_path_destiny.gsub(svn_destiny_name, svn_origin_name)
+           #Check if it doesnt exist in origin
+           if not File.exist?(file_path_origin)
+             #Schedule deletion by svn 
+             puts "The file #{file_path_destiny} does not exist in #{file_path_origin} and so is scheduled to removal in svn"
+             system("svn remove "+file_path_destiny)
+             puts "done removing that file\n"
+             list_of_files_that_should_be_removed.push(file_path_destiny)
+           end          
+         end
+       end                         
+
+       list_of_files_that_should_be_removed.each do |name|
+         puts "file #{name} was removed"
+         system("rm -Rf #{name}")
+       end                                                
+
+       puts "\n--> Done removing files.\n"      
+     end
+     
+     def remove_SVN_files_from_origin() 
+       # Find all .svn in origin and delete them 
+       puts "\n--> Removing .svn files from origin\n"
+       Dir.glob("/tmp/#{@svn_origin_name}/**/.svn", File::FNM_DOTMATCH) do |file_path_origin|                                    
+         puts "Removing .svn file from"+ file_path_origin
+         system("rm -Rf #{file_path_origin}")         
+       end   
+       puts "\n--> End removing files from origin\n"      
+     end 
+     
+     
+     def copy_files_from_origin_to_destiny()
+       #  Copy files that are in origin to destiny.
+       puts "\n--> Copying files from origin to destiny\n"        
+       Dir.glob(File.join("/tmp/#{@svn_origin_name}", '**', '*')) do |file_path_origin|                                            
+         if not file_path_origin.include?(".svn")                                                
+           file_path_destiny = file_path_origin.gsub(svn_origin_name, svn_destiny_name)        
+           puts "Copying file from "+file_path_origin+"     to destiny: "+file_path_destiny
+           if File.directory?(file_path_origin)
+             system("mkdir #{file_path_destiny}")
+             puts "Done creating directory #{file_path_destiny} \n"
+           else
+             system("cp #{file_path_origin} #{file_path_destiny}")  
+             puts "Done copying file to #{file_path_destiny} \n"       
+           end
+         end                                                    
+       end                                             
+       puts "\n--> End copying files\n"             
+     end
+     
+     
+     def check_if_this_is_a_phantom_commit()               
+       # system("cd /tmp/"+@svn_destiny_name +" && "+"svn status | grep '^\?' | awk '{print $2}' | xargs svn add"+" && svn commit -m '"+revision_number_that_we_want_to_copy_to_destiny+"'"+ " && "+"svn update") 
+       if no_changes_to_update?()
+         puts "--> This is a phantom commit. This means that the previous commit and this have exactly the same files. It is not possible to do a commit that doesn't change anything. This either means that either there was an svn error, check the current directory scheme of origin and destiny, or that you just commited a .gitignore file. So we are adding a file 'github_phantom_file' to the repo. This happens when you are using Git and push just a change to .gitignore. Github removes that kind of file, but still make it a new revision."
+         system("touch /tmp/#{@svn_destiny_name}/github_phantom_file")
+       end       
+      
+     end                                          
            
      def perform_conversion_operations(revision_number_that_we_want_to_copy_to_destiny)  
+       
+      # Checkout a temporal repo that will contain the changes –– revision – that we want to apply to destiny
       checkout_origin_repo(revision_number_that_we_want_to_copy_to_destiny)                                                                    
+                          
+      # In the event that the next revision removed files, we should 'svn rm name' before doing the copy.
+      remove_files_from_destiny_repo_that_were_removed_in_origin_repo()                                  
       
-      # SVN Remove files that are in destiny but are not in origin 
-      list_of_files_that_should_be_removed = []
-      puts "\n--> Removing files that exist in destiny but are not in origin, which means they were removed and should be scheduled svn rm 'file'\n"
-      Dir.glob(File.join("/tmp/#{@svn_destiny_name}", '**', '*')) do |file_path_destiny|
-        if not file_path_destiny.include?(".svn")
-          file_path_origin = file_path_destiny.gsub(svn_destiny_name, svn_origin_name)
-          #Check if it doesnt exist in origin
-          if not File.exist?(file_path_origin)
-            #Schedule deletion by svn 
-            puts "The file #{file_path_destiny} does not exist in #{file_path_origin} and so is scheduled to removal in svn"
-            system("svn remove "+file_path_destiny)
-            puts "done removing that file\n"
-            list_of_files_that_should_be_removed.push(file_path_destiny)
-          end          
-        end
-      end                         
+      # SVN files like '.svn' contain info about the state of the repo, revision, changes etc. It must not be 
+      # copied to the destiny repo or we would be overwriting the destiny repo own '.svn' files. 
+      # So we remove them from the temporal origin repo before copying from Origin to Destiny. 
+      remove_SVN_files_from_origin()                                                   
       
-      list_of_files_that_should_be_removed.each do |name|
-        puts "file #{name} was removed"
-        system("rm -Rf #{name}")
-      end                                                
-      
-      puts "\n--> Done removing files.\n"
-          
-      # Find all .svn in origin and delete them 
-      puts "\n--> Removing .svn files from origin\n"
-      Dir.glob("/tmp/#{@svn_origin_name}/**/.svn", File::FNM_DOTMATCH) do |file_path_origin|                                    
-        puts "Removing .svn file from"+ file_path_origin
-        system("rm -Rf #{file_path_origin}")         
-      end   
-      puts "\n--> End removing files from origin\n"                        
-            
-      #  Copy files that are in origin to destiny.
-      puts "\n--> Copying files from origin to destiny\n"        
-      Dir.glob(File.join("/tmp/#{@svn_origin_name}", '**', '*')) do |file_path_origin|                                            
-        if not file_path_origin.include?(".svn")                                                
-          file_path_destiny = file_path_origin.gsub(svn_origin_name, svn_destiny_name)        
-          puts "Copying file from "+file_path_origin+"     to destiny: "+file_path_destiny
-          if File.directory?(file_path_origin)
-            system("mkdir #{file_path_destiny}")
-            puts "Done creating directory #{file_path_destiny} \n"
-          else
-            system("cp #{file_path_origin} #{file_path_destiny}")  
-            puts "Done copying file to #{file_path_destiny} \n"       
-          end
-        end                                                    
-      end                                             
-      puts "\n--> End copying files\n" 
-      
+      # We copy all files from origin repo to Destiny repo. This essencially makes the working directory in destiny identical 
+      # to the working directory of origin, whose state is the the revision that we want to apply. 
+      # Later we just have to svn add * everything. SVN will automatically add what changed.
+      copy_files_from_origin_to_destiny()
+                                                    
+      # Show the current directory layout in origin and destiny
+      # If they are the same, it means the we have a perfect copy. 
       puts "\n\n ======= Current origin schema ======\n"
-      list_directory("/tmp/#{svn_origin_name}")
+      list_directory("/tmp/#{svn_origin_name}")           
       puts "\n\n ======= Current destiny schema ======\n" 
       list_directory("/tmp/#{svn_destiny_name}")                 
-                                 
+                                                          
       # Check if it is a phantom commit. Phantom commits are commit that had just changes to .gitignore file. 
       # Github removes that file from the svn revision, so we are left with two subsequent revisions that are exactly 
       # the identical. SVN commit with changes won't do anything. 
       # To evade this problem, we make a fake file, .github file, so we have something to commit. 
-      # It will be erased in the next commit, so it won't cause any problem.  
-      
-      # system("cd /tmp/"+@svn_destiny_name +" && "+"svn status | grep '^\?' | awk '{print $2}' | xargs svn add"+" && svn commit -m '"+revision_number_that_we_want_to_copy_to_destiny+"'"+ " && "+"svn update") 
-      if no_changes_to_update?()
-        puts "--> This is a phantom commit. This means that the previous commit and this have exactly the same files. It is not possible to do a commit that doesn't change anything. This either means that either there was an svn error, check the current directory scheme of origin and destiny, or that you just commited a .gitignore file. So we are adding a file 'github_phantom_file' to the repo. This happens when you are using Git and push just a change to .gitignore. Github removes that kind of file, but still make it a new revision."
-        system("touch /tmp/#{@svn_destiny_name}/github_phantom_file")
-      end
-
+      # It will be erased in the next commit, so it won't cause any problem.
+      check_if_this_is_a_phantom_commit()      
+                      
+      # The final stage is to 'svn add *' everything and commit to destiny online repo. If this goes well, we 
+      # would have succefully commited the intended revision.
       puts "\n--> We start copy and write process\n"         
       system("cd /tmp/"+@svn_destiny_name +" && "+"svn status | grep '^\?' | awk '{print $2}' | xargs svn add "+ "&& svn commit -m '"+revision_number_that_we_want_to_copy_to_destiny+"'"+ " && "+"svn update")
       puts "\n-->End copy and paste process\n\n" 
